4cb4ae2459db0323237e62d5717f9c30525a16ea
Vinay P <pvinayvinu20@gmail.com>
2022-12-01 17:39:25 +0530

    log backup directory

--- main.sh
+++ main.sh
@@ -6,3 +6,6 @@ BACKUP_DIR="backups"
 mongodump --host $INPUT_MONGODB_SERVER \
 --ssl  --username $INPUT_MONGODB_USER --password $INPUT_MONGODB_PAS \
 --authenticationDatabase admin --db  $INPUT_MONGODB_NAME --archive=./$BACKUP_DIR/dump_`date "+%Y-%m-%d"`.tgz
+
+  echo "🔍 Show me backups... 😎"
+  ls -lFhS ./$BACKUP_DIR/
