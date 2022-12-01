3411cfac5b54de4ec3bd645970a479bdb51d0861
Vinay P <pvinayvinu20@gmail.com>
2022-12-01 16:33:22 +0530

    move archive to 'backups' folder

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+BACKUP_DIR="backups"
+  if [ ! -d ./$BACKUP_DIR/ ]; then
+    mkdir $BACKUP_DIR
+  fi
 mongodump --host $INPUT_MONGODB_SERVER \
 --ssl  --username $INPUT_MONGODB_USER --password $INPUT_MONGODB_PAS \
---authenticationDatabase admin --db  $INPUT_MONGODB_NAME --gzip --archive >  dump_`date "+%Y-%m-%d"`.gz
+--authenticationDatabase admin --db  $INPUT_MONGODB_NAME --gzip --archive >  ./$BACKUP_DIR/dump_`date "+%Y-%m-%d"`.tgz
