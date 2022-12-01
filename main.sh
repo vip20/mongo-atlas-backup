60589d8f392c9d7eb6c835f7b5e03d2a4f74fcec
Vinay P <pvinayvinu20@gmail.com>
2022-12-01 17:11:30 +0530

    update archive command

--- main.sh
+++ main.sh
@@ -5,4 +5,4 @@ BACKUP_DIR="backups"
   fi
 mongodump --host $INPUT_MONGODB_SERVER \
 --ssl  --username $INPUT_MONGODB_USER --password $INPUT_MONGODB_PAS \
---authenticationDatabase admin --db  $INPUT_MONGODB_NAME --gzip --archive >  ./$BACKUP_DIR/dump_`date "+%Y-%m-%d"`.tgz
+--authenticationDatabase admin --db  $INPUT_MONGODB_NAME --archive=./$BACKUP_DIR/dump_`date "+%Y-%m-%d"`.tgz
